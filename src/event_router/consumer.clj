cce43623ad322190594214fdc472c076cdc531a6
Dan Belwood <dan.belwood@gmail.com>
2015-06-16 09:30:12 -0500

    Move consumer code to separate namespace

--- src/event_router/consumer.clj
+++ src/event_router/consumer.clj
@@ -1,33 +1,14 @@
-(ns event-stream.core
-  (:require [clj-kafka.producer :as kp]
-            [clj-kafka.zk :as kzk]
-            [clj-kafka.consumer.simple :as kc]
-            [clj-kafka.core :as k]
-            [zookeeper :as zk])
-  (:import [clj_kafka.core KafkaMessage]))
+(ns event-router.consumer
+  (:require [clj-kafka.consumer.zk :as zk])
+  (:require [clj-kafka.core :as core]))
 
-  (defn- read-loop
+(defn consume-message [consumer-config topic]
+  "Consume 1 message from a topic"
+  (core/with-resource [c (zk/consumer consumer-config)]
+    zk/shutdown
+    (first (zk/messages c topic))))
 
-    )
-
-  (defn start-consumer
-    "Start a consumer for a specific group/topic"
-    [topic partition group-id]
-    (let [kpartition (Integer/parseInt partition)
-          commit-interval (:commit-interval defaults)
-          chan-capacity (:chan-capacity defaults)
-          client-id "event-stream-consumer"
-          m {"zookeeper.connect" (:zookeeper defaults)}
-          partitions (kzk/partitions m topic)
-          ch (chan chan-capacity)
-          pending-messages (atom {})
-          pending-commits (atom (sorted-set))
-          reader-fut (future (read-loop m client-id group-id topic partitions kpartition task-map ch pending-commits))]
-      {:channel ch
-       :reader-future reader-fut
-       :pending-messages pending-messages
-       :pending-commits pending-commits
-       :drained (atom false)}))
-
-  // Shutdown consumer
-)
+(defn get-topic-stream [consumer-config topic]
+  "Continually read messages for a topic"
+    (let [c (zk/consumer consumer-config)]
+      (zk/stream-seq (zk/create-message-stream c topic))))
--- src/event_router/core.clj
+++ src/event_router/core.clj
@@ -1,13 +1,51 @@
-(ns event-stream.core
+(ns event-router.core
   (:use [clj-kafka.producer])
   (:use [clj-kafka.zk])
+  (:use [clj-kafka.core])
+  (:use [clj-kafka.admin])
+  (:require [clojure.string :as str])
+  (:require [event-router.consumer :as consumer])
   (:gen-class))
 
-(def broker-ids (brokers {"zookeeper.connect" "127.0.0.1:2181"}))
+;; Configuration
+(def zookeeper-connect {"zookeeper.connect" "127.0.0.1:2181"})
+(def broker-addresses (str/join ", " (
+                       map
+                       (partial str/join ":")
+                       (map
+                        #(map %1 [:host :port])
+                        (into () (brokers zookeeper-connect))))))
+(def producer-config {"metadata.broker.list" broker-addresses
+                      "serializer.class" "kafka.serializer.DefaultEncoder"
+                      "partitioner.class" "kafka.producer.DefaultPartitioner"})
 
-(def p (producer {"metadata.broker.list" "127.0.0.1:9092"
-                  "serializer.class" "kafka.serializer.DefaultEncoder"
-                  "partitioner.class" "kafka.producer.DefaultPartitioner"}))
+(def consumer-config (conj zookeeper-connect {"group.id" "event-router"
+                                              "auto.offset.reset" "smallest"
+                                              "auto.commit.enable" "false"}))
+(def p (producer producer-config))
+
+
+(defn- publish-message
+  [topic msg]
+  "Publish a message on a topic"
+  (let [message-bytes (.getBytes msg)]
+  (send-message p (message topic message-bytes))))
+
+(def topic-does-not-exist? (complement topic-exists?))
+
+(defn- create-topics
+  [topics]
+  "Given a list of topics, create the ones that don't exist and return the created topics"
+  (let [zk (zk-client (get zookeeper-connect "zookeeper.connect"))
+        exists? (partial topic-does-not-exist? zk)
+        topics-to-create (filter exists? topics)
+        create (partial create-topic zk)]
+    (if (every? true? (map create topics-to-create)) :ok :error)))
+
 (defn -main
   [& args]
-  (send-message p (message "test" (.getBytes "this is my message"))))
+  "Publish and consume a message"
+  (let [topics ["source"]]
+        (create-topics topics)
+        (publish-message "source" "test")))
+        ;;(consumer/consume-message "source")))
