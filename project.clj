795e13ea1142122c1a2a465da7925c000fcae171
Dan Belwood <dan.belwood@gmail.com>
2015-06-16 23:09:40 -0500

    Add json support

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-kafka "0.3.1"]]
+                 [clj-kafka "0.3.1"]
+                 [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot event-router.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
